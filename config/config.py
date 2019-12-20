8603d67bc72e06b101c33fe17c2a04b0f0ed5c21
NGAAI <elizahmuthoni@gmail.com>
2019-12-20 20:17:05 +0300

    mbbm

--- config/config.py
+++ config/config.py
@@ -16,3 +16,9 @@ class Development():
     SECRET_KEY = os.urandom(16).hex()
     DEBUG = True
     SQLALCHEMY_ECHO = True
+
+class Production():
+    SQLALCHEMY_DATABASE_URI = 'postgres://lqhmckjtescrdd:c5c24c71bf040dd26e768204c29971da8ee7534a60914bdc596b993ae5df777a@ec2-174-129-33-186.compute-1.amazonaws.com:5432/d2i1laq46b11tl'
+    SECRET_KEY = os.urandom(16).hex()
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
